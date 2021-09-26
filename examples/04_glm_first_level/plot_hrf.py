e5fdabdfb67d2b3a9ec716cec42fbe43afce8535
alexis <alexisthual@gmail.com>
2021-09-26 15:22:18 +0200

    Fix mion response function example (correct params + flake8)

--- examples/04_glm_first_level/plot_hrf.py
+++ examples/04_glm_first_level/plot_hrf.py
@@ -3,18 +3,23 @@ Example of MRI response functions
 =================================
 
 Within this example we are going to plot the hemodynamic response function
-(:term:`HRF`) model in :term:`SPM` together with the :term:`HRF` shape proposed by G.Glover, as well as
+(:term:`HRF`) model in :term:`SPM` together with the :term:`HRF` shape
+proposed by G.Glover, as well as
 their time and dispersion derivatives.
 We also illustrate how users can input a custom response function,
 which can for instance be useful when dealing with non human primate data
-acquired using a contrast agent. In our case, we input a custom response function
-for MION, a common `agent <https://en.wikipedia.org/wiki/MRI_contrast_agent>`_ used to enhance contrast on MRI images of monkeys.
+acquired using a contrast agent. In our case, we input a custom response
+function for MION, a common
+`agent <https://en.wikipedia.org/wiki/MRI_contrast_agent>`_ used to
+enhance contrast on MRI images of monkeys.
 
-The :term:`HRF` is the filter which couples neural responses to the metabolic-related
-changes in the MRI signal. :term:`HRF` models are simply phenomenological.
+The :term:`HRF` is the filter which couples neural responses to the
+metabolic-related changes in the MRI signal. :term:`HRF` models
+are simply phenomenological.
 
-In current analysis frameworks, the choice of :term:`HRF` model is essentially left to
-the user. Fortunately, using the :term:`SPM` or Glover model does not make a huge
+In current analysis frameworks, the choice of :term:`HRF` model is essentially
+left to the user. Fortunately, using the :term:`SPM` or
+Glover model does not make a huge
 difference. Adding derivatives should be considered whenever timing
 information has some degree of uncertainty, and is actually useful to detect
 timing issues.
@@ -41,23 +46,94 @@ stim = np.zeros_like(frame_times)
 stim[(frame_times > onset) * (frame_times <= onset + duration)] = amplitude
 
 #########################################################################
-# Define custom response function for MION:
+# Define custom response functions for MION:
 from scipy.stats import gamma
 
 
-def mion_response_function(tr, oversampling=50, time_length=32.0, onset=0.0):
+def mion_response_function(
+    tr,
+    oversampling=16,
+    time_length=70.0,
+    onset=0.0,
+    delay=1.55,
+    dispersion=5.5,
+):
+    """Implementation of the MION response function model.
+
+    Parameters
+    ----------
+    tr: float
+        scan repeat time, in seconds
+    oversampling: int, optional
+        temporal oversampling factor
+    time_length: float, optional
+        hrf kernel length, in seconds
+    onset: float, optional
+        hrf onset time, in seconds
+    delay: float, optional
+        delay parameter of the hrf (in s.)
+    dispersion: float, optional
+        dispersion parameter for the gamma function
+
+    Returns
+    -------
+    response_function: array of shape(length / tr * oversampling, dtype=float)
+        response_function sampling on the oversampled time grid
+    """
     dt = tr / oversampling
     time_stamps = np.linspace(
         0, time_length, np.rint(float(time_length) / dt).astype(int)
     )
     time_stamps -= onset
 
-    delay = 5
-
-    response_function = gamma.pdf(time_stamps, delay, scale=4)
+    response_function = gamma.pdf(time_stamps, delay, loc=0, scale=dispersion)
     response_function /= response_function.sum()
+    response_function *= -1
 
     return response_function
+
+
+def mion_time_derivative(
+    tr,
+    oversampling=16.0,
+    time_length=70.0,
+    onset=0.0,
+    delay=1.55,
+    dispersion=5.5,
+):
+    """Implementation of the MION time derivative response function model.
+
+    Parameters
+    ----------
+    tr: float
+        scan repeat time, in seconds
+    oversampling: int, optional
+        temporal oversampling factor, optional
+    time_length: float, optional
+        hrf kernel length, in seconds
+    onset: float, optional
+        onset of the response in seconds
+    delay: float, optional
+        delay parameter of the hrf (in s.)
+    dispersion: float, optional
+        dispersion parameter for the gamma function
+
+    Returns
+    -------
+    drf: array of shape(time_length / tr * oversampling, dtype=float)
+        derived_response_function sampling on the provided grid
+    """
+    do = 0.1
+    drf = (
+        mion_response_function(
+            tr, oversampling, time_length, onset, delay, dispersion
+        )
+        - mion_response_function(
+            tr, oversampling, time_length, onset + do, delay, dispersion
+        )
+    ) / do
+
+    return drf
 
 
 #########################################################################
@@ -67,6 +143,7 @@ rf_models = [
     ("spm + derivative + dispersion", "SPM HRF"),
     ("glover + derivative + dispersion", "Glover HRF"),
     (mion_response_function, "Mion RF"),
+    (mion_time_derivative, "Mion RF derivative"),
 ]
 
 #########################################################################
@@ -76,11 +153,19 @@ rf_models = [
 import matplotlib.pyplot as plt
 from nilearn.glm.first_level import compute_regressor
 
+oversampling = 16
+time_length = 50.0
+
 fig = plt.figure(figsize=(9, 4))
 for i, (rf_model, model_title) in enumerate(rf_models):
     # compute signal of interest by convolution
     signal, labels = compute_regressor(
-        exp_condition, rf_model, frame_times, con_id="main", oversampling=16
+        exp_condition,
+        rf_model,
+        frame_times,
+        con_id="main",
+        oversampling=oversampling,
+        time_length=time_length,
     )
 
     # plot signal
